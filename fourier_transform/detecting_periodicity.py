290f68dbcbb84e92d7d0c8600d82456aa07b7df3
Farias123 <vitorcostafarias@gmail.com>
2024-09-25 00:40:16 -0300

    exec 7.2

---
+++ fourier_transform/detecting_periodicity.py
@@ -0,0 +1,26 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from simple_fuctions import dft, num_coef
+
+
+def main():
+    with open('sunspots.txt', 'r') as file:
+        content = file.read()
+
+    data = [x.split('\t') for x in content.split('\n')]
+    months = [float(x[0]) for x in data]
+    spots = [float(x[1]) for x in data]
+
+
+    fig, ax = plt.subplots()
+    ax.scatter(months, spots, s=1, marker='o')
+    ax.set_yticks(np.arange(min(spots), max(spots), 10))
+    plt.show()
+
+    c = dft(months)
+    fig, ax = plt.subplots()
+    ax.scatter(range(num_coef(len(months))), abs(c)**2, marker='o', s=1) # todo apenas um pico
+    plt.show()
+
+if __name__ == '__main__':
+    main()
